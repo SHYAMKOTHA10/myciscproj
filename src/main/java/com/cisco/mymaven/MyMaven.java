d8a448a1c91768d19e26b5603cfa73470f8e1a2b
Shyam Kotha (shykotha) <shykotha@cisco.com>
2017-03-10 16:15:36 +0530

    commit
    commit

---
+++ src/main/java/com/cisco/mymaven/MyMaven.java
@@ -0,0 +1,10 @@
+package com.cisco.mymaven;
+
+public class MyMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("I am Good");
+	}
+
+}
